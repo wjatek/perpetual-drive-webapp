85dcb8c603ec1341be10cc1926e68d2d5767fd91
wjatek <kontakt@wjatek.pl>
2024-12-15 13:25:05 +0100

    add basic dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,36 +1,94 @@
+'use client'
+
+import MenuIcon from '@mui/icons-material/Menu'
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  useMediaQuery,
+} from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { useState } from 'react'
+
+const drawerWidth = 240
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <div>
-      {/* Sidebar */}
-      <aside>
-        <h2>Dashboard</h2>
-        <nav>
-          <ul>
-            <li>
-              <a href="/dashboard">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/dashboard/wall">
-                Wall
-              </a>
-            </li>
-            <li>
-              <a href="/dashboard/drive">
-                Drive
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </aside>
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const [mobileOpen, setMobileOpen] = useState(false)
 
-      {/* Main Content */}
-      <main>{children}</main>
-    </div>
+  const toggleDrawer = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  return (
+    <Box>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          variant={isMobile ? 'temporary' : 'permanent'}
+          open={isMobile ? mobileOpen : true}
+          onClose={toggleDrawer}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+        >
+          <Toolbar />
+          <List>
+            <ListItem component="a" href={`/dashboard`}>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem component="a" href={`/dashboard/wall`}>
+              <ListItemText primary="Wall" />
+            </ListItem>
+            <ListItem component="a" href={`/dashboard/drive`}>
+              <ListItemText primary="Drive" />
+            </ListItem>
+          </List>
+        </Drawer>
+
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+          <Toolbar
+            sx={{
+              position: isMobile ? 'fixed' : 'sticky',
+              top: 0,
+              width: '100%',
+              zIndex: 1,
+            }}
+          >
+            {isMobile && (
+              <IconButton
+                color="inherit"
+                edge="start"
+                onClick={toggleDrawer}
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
+            <Box sx={{ flexGrow: 1 }} />
+          </Toolbar>
+
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            {children}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
 }
