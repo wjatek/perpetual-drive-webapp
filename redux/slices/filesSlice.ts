40beb3fee7dbaa3ee9d69fb0c1cb7d92952ba144
wjatek <kontakt@wjatek.pl>
2025-01-01 18:33:14 +0100

    add better downloading indicator, handle downloading state globally

--- redux/slices/filesSlice.ts
+++ redux/slices/filesSlice.ts
@@ -1,18 +1,22 @@
 import api from '@/lib/api'
 import { createSlice } from '@/redux/createAppSlice'
 import { File } from '@/types/models'
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
 
 interface FilessState {
   filesByDirectoryId: { [key: string | '']: File[] }
   loading: { [key: string | '']: boolean }
   error: string | null
+  fileDownloading: { [key: string | '']: boolean }
+  fileDownloadingProgress: { [key: string | '']: number }
 }
 
 const initialState: FilessState = {
   filesByDirectoryId: {},
   loading: {},
   error: null,
+  fileDownloading: {},
+  fileDownloadingProgress: {},
 }
 
 export const fetchFiles = createAsyncThunk<File[], string | null>(
@@ -26,7 +30,22 @@ export const fetchFiles = createAsyncThunk<File[], string | null>(
 const filesSlice = createSlice({
   name: 'files',
   initialState,
-  reducers: {},
+  reducers: {
+    setFileDownloading(
+      state,
+      action: PayloadAction<{ id: string; isDownloading: boolean }>
+    ) {
+      const { id, isDownloading } = action.payload
+      state.fileDownloading[id] = isDownloading
+    },
+    setFileDownloadingProgress(
+      state,
+      action: PayloadAction<{ id: string; value: number }>
+    ) {
+      const { id, value } = action.payload
+      state.fileDownloadingProgress[id] = value
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchFiles.pending, (state, action) => {
@@ -48,4 +67,6 @@ const filesSlice = createSlice({
   },
 })
 
+export const { setFileDownloading, setFileDownloadingProgress } =
+  filesSlice.actions
 export default filesSlice.reducer
